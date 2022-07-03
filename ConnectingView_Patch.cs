ac0d8d1358c7704aca9b5845ef90ac37b2b3325c
iZastic <izastic@gmail.com>
2022-07-02 19:11:29 -0600

    Initial commit

---
+++ ConnectingView_Patch.cs
@@ -0,0 +1,19 @@
+﻿using HarmonyLib;
+using ProjectM.UI;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+namespace AutoReady;
+
+[HarmonyPatch]
+internal static class ConnectingView_Patch
+{
+    [HarmonyPostfix]
+    [HarmonyPatch(typeof(ConnectingView), nameof(ConnectingView.Update))]
+    private static void Update(ConnectingView __instance)
+    {
+        Plugin.Logger.LogMessage("Trying to skip");
+        if (__instance._Ready)
+            __instance.BackgroundButton_OnClick();
+    }
+}
---
+++ README.md
@@ -0,0 +1,15 @@
+# AutoReady
+Automatically skips the ready screen when loaded
+
+### Installation
+- Install [BepInEx](https://v-rising.thunderstore.io/package/BepInEx/BepInExPack_V_Rising/)
+- Extract _AutoReady.dll_ into _(VRising folder)/BepInEx/plugins_
+
+### Support
+Join the [V Rising Mod Community](https://discord.gg/r87Vdez2Br), and ping `@iZastic#0365`.
+
+Submit a ticket on [GitHub](https://github.com/iZastic/vrising-autoready/issues)
+
+### Changelog
+`1.0.0`
+- Initial upload
