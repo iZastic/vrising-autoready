99fd9d128cc15fe50afd85bf28e041ddd26188ba
iZastic <izastic@gmail.com>
2022-07-02 19:13:51 -0600

    Forgot to remove logging

--- ConnectingView_Patch.cs
+++ ConnectingView_Patch.cs
@@ -1,7 +1,5 @@
 ﻿using HarmonyLib;
 using ProjectM.UI;
-using UnityEngine;
-using UnityEngine.Rendering;
 
 namespace AutoReady;
 
@@ -12,7 +10,6 @@ internal static class ConnectingView_Patch
     [HarmonyPatch(typeof(ConnectingView), nameof(ConnectingView.Update))]
     private static void Update(ConnectingView __instance)
     {
-        Plugin.Logger.LogMessage("Trying to skip");
         if (__instance._Ready)
             __instance.BackgroundButton_OnClick();
     }
